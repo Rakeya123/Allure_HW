dcede78901fd89c92e81e88da818e73a165a0a84
Anna Usolskaya <rakeya@yandex.ru>
2024-04-12 12:22:59 +0300

    realise 2

--- src/test/java/qa/guru/allure/StepsTest.java
+++ src/test/java/qa/guru/allure/StepsTest.java
@@ -1,7 +1,6 @@
 package qa.guru.allure;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -12,7 +11,7 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.attachment;
 import static io.qameta.allure.Allure.step;
-import static org.openqa.selenium.By.linkText;
+
 
 public class StepsTest extends TestBase{
 
