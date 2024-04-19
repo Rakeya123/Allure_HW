41829f721dc970612a30ba62a3777d8cce9a1402
Anna Usolskaya <rakeya@yandex.ru>
2024-04-19 17:47:04 +0300

    bot settings TAG1

--- src/test/java/qa/guru/allure/StepsTest.java
+++ src/test/java/qa/guru/allure/StepsTest.java
@@ -1,12 +1,17 @@
 package qa.guru.allure;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
@@ -15,6 +20,23 @@ import static io.qameta.allure.Allure.step;
 
 @Tag("minimum")
 public class StepsTest extends TestBase{
+
+    @BeforeAll
+    static void setup() {
+
+
+        Configuration.remote = "https://user1:1234@"+System.getProperty("remote","selenoid.autotests.cloud")+"/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+    }
 
     @Test
     @Feature("Issue в репозитории")
